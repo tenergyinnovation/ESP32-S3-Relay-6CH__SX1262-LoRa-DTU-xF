bfd34bc1da917ac0afa34ddb47a840729538e624
Tenergy Innovation <45764087+tenergyinnovation@users.noreply.github.com>
2026-06-29 23:22:01 +0700

    Update repository URL for ESP32-S3-Relay-6CH

--- lib/clone_repo.sh
+++ lib/clone_repo.sh
@@ -69,7 +69,7 @@ LIBRARY_URLS=(
     "https://github.com/bblanchon/ArduinoJson.git"
     "https://github.com/knolleary/pubsubclient.git"
     "https://github.com/taranais/NTPClient.git"
-    "https://github.com/tenergyinnovation/ESP32-S3-Relay-6CH__SX1262-LoRa-DTU-xF.git"
+    "https://github.com/tenergyinnovation/ESP32-S3-Relay-6CH.git"
 )
 
 # Optional Libraries (uncomment to enable)
